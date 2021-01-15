227de770f57ffdffaca21243c40e43ad05e4d7ec
Chris Bongers <chrisbongers@gmail.com>
2021-01-15 09:47:15 +0200

    Added Tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#2d334d",
+        "light-blue": "#d5d8e0",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
